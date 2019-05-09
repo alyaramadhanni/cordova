88cf5fc108caa2ad88b9b1113322575f4cc55ddc
alyaramadhanni <48471940+alyaramadhanni@users.noreply.github.com>
2019-05-09 12:46:50 +0700

    Update delete.php

--- www/delete.php
+++ www/delete.php
@@ -3,7 +3,7 @@
  if(isset($_GET['id']))
  {
  $id=$_GET['id'];
- $q=mysqli_query($con,"delete from 'peserta' where 'id_peserta'='$id_peserta'");
+ $q=mysqli_query($con,"delete from `peserta` where `id_peserta`='$id_peserta'");
  if($q)
  echo "success";
  else
