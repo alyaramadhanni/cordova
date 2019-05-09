57d3d55dc2b49c76dd470994e8856d0280ac8e1d
alyaramadhanni <48471940+alyaramadhanni@users.noreply.github.com>
2019-05-09 12:33:35 +0700

    Update db.php

--- www/db.php
+++ www/db.php
@@ -1,4 +1,4 @@
 <?php
  header("Access-Control-Allow-Origin: *");
- $con = mysqli_connect("localhost","id9299562_tugas7","tugas7","id9299562_tugas7") or die ("could not connect database");
+ $con = mysqli_connect("localhost","id8219133_root","rootpkl","id8219133_pkl") or die ("could not connect database");
 ?>
