c0a3a35d55a6219bcbc9da94d8dba370e760208d
alyaramadhanni <48471940+alyaramadhanni@users.noreply.github.com>
2019-05-09 12:51:00 +0700

    Update insert.php

--- www/insert.php
+++ www/insert.php
@@ -5,7 +5,7 @@
  $nim=$_POST['nim'];
  $nama=$_POST['nama'];
  $sertifikasi=$_POST['sertifikasi'];
- $q=mysqli_query($con,"INSERT INTO 'peserta' ('nim','nama','sertifikasi') VALUES ('$nim','$nama','$sertifikasi')");
+ $q=mysqli_query($con,"INSERT INTO `peserta` (`nim`,`nama`,`sertifikasi`) VALUES ('$nim','$nama','$sertifikasi')");
  if($q)
   echo "success";
  else
